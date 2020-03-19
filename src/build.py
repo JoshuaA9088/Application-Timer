82db7efb3163df0a9211fa1bf6957a24a29f2227
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-19 01:57:40 -0600

    Fixed build script

--- src/build.py
+++ src/build.py
@@ -1,5 +1,5 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run(
-    ["--name=ApplicationTimer", "--onefile", "--windowed", "timer.py"]
+    ["--name=ApplicationTimer", "--onefile", "--windowed", "src/timer.py"]
 )
