f6756f52ea236ef53e42a122b365f6c75cf0bfac
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-19 01:38:20 -0600

    Cleaned up with pre-commit.

--- src/timer.py
+++ src/timer.py
@@ -1,21 +1,21 @@
-import time
-import subprocess
-import psutil
-import tkinter as tk
-import os
 import configparser
 import csv
 import datetime
+import os
+import subprocess
+import time
+import tkinter as tk
 from pathlib import Path
+
+import psutil
 
 FMT = "%H:%M:%S"
 
 
 class ApplicationTimer(object):
-
     def __init__(self):
         super().__init__()
-        self.fields = ('Name', 'Department')
+        self.fields = ("Name", "Department")
         self.title = "Application Timer"
 
         self.load_config()
@@ -23,9 +23,10 @@ class ApplicationTimer(object):
         self.center()
         self.entries["Name"].icursor(0)
         start_button = tk.Button(
-            self.root, text='Start', command=(lambda e=self.entries: self.time_app(e)))
+            self.root, text="Start", command=(lambda e=self.entries: self.time_app(e))
+        )
         start_button.pack(side=tk.RIGHT, padx=5, pady=5)
-        quit_button = tk.Button(self.root, text='Quit', command=self.root.quit)
+        quit_button = tk.Button(self.root, text="Quit", command=self.root.quit)
         quit_button.pack(side=tk.RIGHT, padx=5, pady=5)
         self.root.mainloop()
 
@@ -37,16 +38,19 @@ class ApplicationTimer(object):
         # ConfigPath = my_docs
         if not os.path.exists(my_docs):
             os.mkdir(my_docs)
-        config['DEFAULT'] = {'ExecutablePath': r"C:\Windows\System32\notepad.exe",
-                             'OutputLogPath': os.path.join(my_docs, "timer_log.csv")}
+        config["DEFAULT"] = {
+            "ExecutablePath": r"C:\Windows\System32\notepad.exe",
+            "OutputLogPath": os.path.join(my_docs, "timer_log.csv"),
+        }
 
         if os.path.exists(os.path.join(my_docs, "timer.ini")):
             config.read(os.path.join(my_docs, "timer.ini"))
             self.exe_path = config["DEFAULT"]["ExecutablePath"]
             self.log_path = config["DEFAULT"]["OutputLogPath"]
         else:
-            self.exe_path = r"C:\Windows\System32\notepad.exe " + \
-                os.path.join(my_docs, "timer.ini")
+            self.exe_path = r"C:\Windows\System32\notepad.exe " + os.path.join(
+                my_docs, "timer.ini"
+            )
             self.log_path = os.path.join(my_docs, "timer_log.csv")
             # f = open(os.path.join(my_docs, "timer.ini"), "w+")
             with open(Path(my_docs, "timer.ini"), "w+") as f:
@@ -58,17 +62,12 @@ class ApplicationTimer(object):
 
         for field in self.fields:
             row = tk.Frame(self.root)
-            lab = tk.Label(row, width=22, text=field + ": ", anchor='w')
+            lab = tk.Label(row, width=22, text=field + ": ", anchor="w")
             ent = tk.Entry(row)
             # ent.insert(0, "0")
-            row.pack(side=tk.TOP,
-                     fill=tk.X,
-                     padx=5,
-                     pady=5)
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
             lab.pack(side=tk.LEFT)
-            ent.pack(side=tk.RIGHT,
-                     expand=tk.YES,
-                     fill=tk.X)
+            ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
             self.entries[field] = ent
 
     def center(self):
@@ -78,7 +77,7 @@ class ApplicationTimer(object):
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
 
-        size = tuple(int(_) for _ in self.root.geometry().split('+')[0].split('x'))
+        size = tuple(int(_) for _ in self.root.geometry().split("+")[0].split("x"))
         x = screen_width / 2 - size[0] / 2
         y = screen_height / 2 - size[1] / 2
 
@@ -89,15 +88,17 @@ class ApplicationTimer(object):
         if os.path.exists(self.log_path):
             csvData = []
         else:
-            csvData = [["Date", "Name", "Department",
-                        "Start Time", "End Time", "Elapsed"]]
+            csvData = [
+                ["Date", "Name", "Department", "Start Time", "End Time", "Elapsed"]
+            ]
 
         date = datetime.datetime.today()
-        date = date.strftime('%m-%d-%Y')
+        date = date.strftime("%m-%d-%Y")
         date = str(date)
 
         elapsed = datetime.datetime.strptime(
-            self.end_time, FMT) - datetime.datetime.strptime(self.start_time, FMT)
+            self.end_time, FMT
+        ) - datetime.datetime.strptime(self.start_time, FMT)
         elapsed = str(elapsed)
 
         csvData.append([date, name, dept, self.start_time, self.end_time, elapsed])
@@ -133,5 +134,5 @@ class ApplicationTimer(object):
         self.write_log(name, dept)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     timer = ApplicationTimer()
